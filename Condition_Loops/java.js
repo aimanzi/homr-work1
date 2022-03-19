b8bea7db00c99728fe69637e633228bbb3898865
ZIDANE Aiman PCC ISRAEL <zidanaima1@ferrero.com>
2022-03-19 21:19:33 +0200

    adding some files

---
+++ Condition_Loops/java.js
@@ -0,0 +1,128 @@
+//קילטו שני מספרים והציגו את הגדול מביניהם. אם שניהם שווים יש להציג אחד מהם בלבד
+// var num1, num2;
+// num1 = +prompt("Enter number");
+// num2 = +prompt("Enter another aumber");
+// if (num1 > num2)
+// {
+// alert(num1);
+// }
+// else if (num1<num2)
+// {
+// alert(num2);
+// }
+// else if (num1=num2)
+// {
+//     alert(num1);
+// }
+
+
+//קילטו ציון והציגו "עובר" אם הציון גדול או שווה ל-60, אחרת הציגו "נכשל"
+// var score;
+// score= +prompt("Enter youe score")
+// if (score>=60)
+// {
+// alert("Pass");  
+// } 
+// else
+// {
+// alert("Failed")  
+// }
+
+
+// קילטו גיל, הציגו הודעה המציינת האם ניתן להוציא רישיון נהיגה או לא
+// אם לא – יש להציג את מספר השנים שיש להמתין
+// var age, gap;
+// age = +prompt("Enter you age");
+// if (age >= 17) {
+//     alert("OK you can issue car license")
+// }
+// else {
+//     gap = 17 - age;
+//     alert("You cant issue a car licaense,Years to issue is: " + gap);
+// }
+
+
+// קילטו מספר, הציגו "חיובי", "שלילי" או "אפס" כתלות במספר
+// var num1;
+// num1 = +prompt("Enter a number");
+// if (num1 >= 1) {
+//     alert("Positive");
+// }
+// else if (num1 < 0) {
+//     alert("Negative");
+// }
+// else {
+//     alert("Zero");
+// }
+
+
+// א.	"המספר הינו מספר שלילי"ב.	"המספר הינו אפס"ג.	"המספר הינו בין 1 ל-100 כולל"ד.	"המספר הינו בין 101 ל-1000 כולל"
+// ה.	"המספר גדול מ-1000"
+// var num1;
+// num1 = +prompt("Enter a number");
+// if (num1 < 0) {
+//     alert("The number is negative")
+// } else if (num1 == 0) 
+//     alert("Zero")
+// } else if (num1>0 && num1<101) {
+//     alert("The number between 1-100");
+// }
+// else if (num1 > 100 && num1 < 1001) {
+//     alert("The number between 101 to 1000");
+// }
+// else {
+//     alert("The number bigger than 1000");
+// }
+
+
+// 6.קילטו ציון והציגו: א.	"נכשל" אם הציון בין 0 ל-59 כולל
+// ב.	"מספיק" אם הציון בין 60 ל-69 כולל.
+// ג.	"כמעט טוב" אם הציון בין 70 ל-79 כולל.
+// ד.	"טוב" אם הציון בין 80 ל-89 כולל.
+// ה.	"טוב מאוד" אם הציון בין 90 ל-99 כולל.
+// ו.	"מעולה" אם הציון 100.
+// var score;
+// score = +prompt("Enter a number");
+// if (score >= 0 && score <= 59 ) {
+//     alert("Failied");
+// }
+// else if (score >=60 && score <=69) {
+//     alert("Enough");
+// }
+// else if (score >=70 && score <=79) {
+//     alert("Almost good");
+// }
+// else if (score >=80 && score <=89) {
+//     alert("Good");
+// }
+// else if (score >=90 && score <= 99) {
+//     alert("Very good");
+// }
+// else if (score == 100) {
+//     alert("Excellent");
+// }
+// else {
+//     alert("Invail score");
+// }
+
+
+// 7.	קילטו שלושה מספרים, הציגו את המקסימלי מביניהם.
+// var num1, num2, num3;
+// num1 = +prompt("Enter the first number");
+// num2 = +prompt("Enter the second number");
+// num3 = +prompt("Enter the third number");
+// if (num1 > num2 && num1 > num3) {
+//     alert(num1);
+// } else if (num2 > num1 && num2 > num3) {
+//     alert(num2);
+// }
+// else if(num3>num1 && num3>num2)
+// {
+//     alert(num3);
+// }
+// else{
+//     alert("The numbers is equal");
+// }
+
+
+
